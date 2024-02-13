90920b60eefd03117c75cb6e7ad8392f461ba50c
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-02-13 18:50:35 +0100

    Add fullscreen mode

--- lib/ui/video_player.dart
+++ lib/ui/video_player.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:auto_orientation/auto_orientation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fvp/fvp.dart';
 import 'package:video_player/video_player.dart';
 
@@ -16,6 +18,7 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   late VideoPlayerController controller;
   Timer? hideControlsTimer;
   bool showControls = true;
+  bool isFullScreen = false;
 
   @override
   void initState() {
@@ -80,60 +83,105 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
     });
   }
 
+  void toggleFullScreen() {
+    setState(() {
+      isFullScreen = !isFullScreen;
+      if (isFullScreen) {
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+        AutoOrientation.landscapeAutoMode(forceSensor: true);
+      } else {
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+        AutoOrientation.portraitAutoMode();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        behavior: HitTestBehavior.translucent, // pass taps to elements below
-        onTap: showControlsOverlay,
-        child: Column(
+      body: isFullScreen
+          ? RotatedBox(
+              quarterTurns: 4,
+              child: buildVideoPlayer(context),
+            )
+          : buildVideoPlayer(context),
+    );
+  }
+
+  Widget buildVideoPlayer(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent, // pass taps to elements below
+      onTap: showControlsOverlay,
+      // toggle fullscreen when user swipes down or up on video
+      // down only works in fullscreen
+      // up only works in non-fullscreen
+      // TODO: Add nice animation ala youtube app
+      onVerticalDragEnd: (details) {
+        if (details.velocity.pixelsPerSecond.dy * (isFullScreen ? 1 : -1) > 0) {
+          toggleFullScreen();
+        }
+      },
+      child: Container(
+          // add a background to be able to switch to pitch-black when in fullscreen
+          color: isFullScreen ? Colors.black : Colors.transparent,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               // set a fixed height to avoid BoxConstraints errors
               Container(
-                  height: MediaQuery.of(context).size.width * 9 / 16,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      controller.value.isInitialized
-                          ? AspectRatio(
-                              aspectRatio: 16 / 9,
-                              child: VideoPlayer(controller),
-                            )
-                          : const CircularProgressIndicator(),
-                      AnimatedOpacity(
+                height: isFullScreen
+                    ? MediaQuery.of(context).size.height
+                    : MediaQuery.of(context).size.width * 9 / 16,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    controller.value.isInitialized
+                        ? AspectRatio(
+                            aspectRatio: 16 / 9,
+                            child: VideoPlayer(controller),
+                          )
+                        : const CircularProgressIndicator(),
+                    AnimatedOpacity(
+                      opacity: showControls ? 1.0 : 0.0,
+                      duration: const Duration(milliseconds: 220),
+                      child: Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                    IgnorePointer(
+                      ignoring: !showControls,
+                      child: AnimatedOpacity(
                         opacity: showControls ? 1.0 : 0.0,
                         duration: const Duration(milliseconds: 220),
-                        child: Container(
-                          width: double.infinity,
-                          height: double.infinity,
-                          color: Colors.black.withOpacity(0.5),
+                        child: CircleAvatar(
+                          radius: 28,
+                          backgroundColor: Colors.black.withOpacity(0.1),
+                          child: IconButton(
+                            splashColor: Colors.transparent,
+                            icon: Icon(
+                              controller.value.isPlaying
+                                  ? Icons.pause
+                                  : Icons.play_arrow,
+                              size: 40.0,
+                              color: Colors.white,
+                            ),
+                            onPressed: playPausePlayer,
+                          ),
                         ),
                       ),
-                      IgnorePointer(
-                          ignoring: !showControls,
-                          child: AnimatedOpacity(
-                            opacity: showControls ? 1.0 : 0.0,
-                            duration: const Duration(milliseconds: 220),
-                            child: CircleAvatar(
-                                radius: 28,
-                                backgroundColor: Colors.black.withOpacity(0.1),
-                                child: IconButton(
-                                  splashColor: Colors.transparent,
-                                  icon: Icon(
-                                      controller.value.isPlaying
-                                          ? Icons.pause
-                                          : Icons.play_arrow,
-                                      size: 40.0,
-                                      color: Colors.white),
-                                  onPressed: playPausePlayer,
-                                )),
-                          )),
-                      Positioned(
-                          bottom: 5.0,
-                          left: 10.0,
-                          right: 10.0,
-                          child: IgnorePointer(
+                    ),
+                    Positioned(
+                      bottom: 5.0,
+                      left: 20.0,
+                      right: 0.0,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Expanded(
+                                child: IgnorePointer(
                               ignoring: !showControls,
                               child: AnimatedOpacity(
                                 opacity: showControls ? 1.0 : 0.0,
@@ -158,11 +206,30 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
                                   onSeek: (duration) =>
                                       controller.seekTo(duration),
                                 ),
-                              ))),
-                    ],
-                  ))
-            ]),
-      ),
+                              ),
+                            )),
+                            IgnorePointer(
+                                ignoring: !showControls,
+                                child: AnimatedOpacity(
+                                    opacity: showControls ? 1.0 : 0.0,
+                                    duration: const Duration(milliseconds: 220),
+                                    child: IconButton(
+                                      icon: Icon(
+                                        isFullScreen
+                                            ? Icons.fullscreen_exit
+                                            : Icons.fullscreen,
+                                        color: Colors.white,
+                                        size: 30.0,
+                                      ),
+                                      onPressed: toggleFullScreen,
+                                    )))
+                          ]),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
