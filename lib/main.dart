f7be76e47a08addd9ab359a2b9203df0b2bbc95a
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-02-19 19:57:01 +0100

    init pluginmanager after runApp + remove testing code

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'backend/plugin_manager.dart';
 
 void main() {
-  // init the plugin manager
-  PluginManager();
   runApp(const MyApp());
+  PluginManager();
 }
 
 class MyApp extends StatelessWidget {
@@ -21,27 +20,10 @@ class MyApp extends StatelessWidget {
         ? ThemeData.dark().colorScheme
         : ThemeData.light().colorScheme;
 
-    final UniversalVideoMetadata testVideoMetadata = UniversalVideoMetadata(
-      m3u8Uri: Uri.parse(
-          "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
-      title: "Sexy schtuff",
-      pluginOrigin: null,
-    );
-
     return MaterialApp(
       title: 'Simple Video Player',
       theme: ThemeData.from(colorScheme: colorScheme),
       home: const HomeScreen(),
     );
   }
-}
-
-void setSettings() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setBool('dark_theme', true);
-}
-
-void getSetting() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  print(prefs.getBool('dark_theme'));
 }
