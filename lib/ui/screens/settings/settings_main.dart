ce66fcc1651ac6e0525619908acd6cb484a4d8e9
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-03-10 02:56:28 +0100

    Create settings screens

---
+++ lib/ui/screens/settings/settings_main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:hedon_viewer/ui/screens/settings/settings_appearance.dart';
+import 'package:hedon_viewer/ui/screens/settings/settings_plugins.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: _SettingsScreenWidget()),
+    );
+  }
+}
+
+class _SettingsScreenWidget extends StatefulWidget {
+  @override
+  State<_SettingsScreenWidget> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<_SettingsScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
+        title: const Text("Settings"),
+      ),
+      body: SafeArea(
+          child: ListView(
+        children: <Widget>[
+          ListTile(
+            title: const Text("Plugins"),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PluginsScreen()));
+            },
+          ),
+          ListTile(
+            title: const Text("Appearance"),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AppearanceScreen()));
+            },
+          ),
+        ],
+      )),
+    );
+  }
+}
