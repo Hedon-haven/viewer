4403069970e1493aa4c88a80e83ea5e0c561e6da
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-03-15 23:52:13 +0100

    Make OptionsSwitch stateful
    Allows running setState from inside OptionsSwitch to avoid code duplication

--- lib/ui/custom_widgets/options_switch.dart
+++ lib/ui/custom_widgets/options_switch.dart
@@ -16,18 +16,52 @@ class OptionsSwitch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _OptionsSwitchWidget(
+        title: title,
+        subTitle: subTitle,
+        switchState: switchState,
+        onSelected: onSelected);
+  }
+}
+
+class _OptionsSwitchWidget extends StatefulWidget {
+  final String title;
+  final String subTitle;
+  late bool switchState;
+  final void Function(bool) onSelected;
+
+  _OptionsSwitchWidget({
+    required this.title,
+    required this.subTitle,
+    required this.switchState,
+    required this.onSelected,
+  });
+
+  @override
+  State<_OptionsSwitchWidget> createState() => _OptionsSwitchWidgetState();
+}
+
+class _OptionsSwitchWidgetState extends State<_OptionsSwitchWidget> {
+  @override
+  Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
         Expanded(
           child: ListTile(
-            title: Text(title),
-            subtitle: Text(subTitle),
+            title: Text(widget.title),
+            subtitle: Text(widget.subTitle),
           ),
         ),
         Switch(
-          value: switchState,
+          value: widget.switchState,
           onChanged: (value) {
-            onSelected(value);
+            widget.onSelected(value);
+            // The user provided function completes after the setState below
+            // is called -> value is written to settings successfully,
+            // but widget is not updated visually
+            // -> Manually temporarily change switchState to the new value
+            widget.switchState = value;
+            setState(() {});
           },
         ),
       ],
