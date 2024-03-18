7f670b473cd0288e3a3b7319f7e11f4d4a0c9fcc
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-03-18 21:23:09 +0100

    Add homepage + homepage setting

---
+++ lib/ui/screens/settings/settings_homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:hedon_viewer/backend/managers/plugin_manager.dart';
+import 'package:hedon_viewer/main.dart';
+import 'package:hedon_viewer/ui/custom_widgets/options_switch.dart';
+
+class HomepageScreen extends StatelessWidget {
+  const HomepageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _HomePageScreenWidget();
+  }
+}
+
+class _HomePageScreenWidget extends StatefulWidget {
+  @override
+  State<_HomePageScreenWidget> createState() => _HomepageScreenState();
+}
+
+class _HomepageScreenState extends State<_HomePageScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme:
+              IconThemeData(color: Theme.of(context).colorScheme.primary),
+          title: const Text("Homepage"),
+        ),
+        body: SafeArea(
+            child: Column(children: <Widget>[
+          ListView(children: [
+            OptionsSwitch(
+                title: "Enable Homepage",
+                subTitle: "Enable Homepage on app startup",
+                switchState: sharedStorage.getBool("homepage_enabled")!,
+                onSelected: (value) =>
+                    sharedStorage.setBool("homepage_enabled", value))
+          ])
+        ])));
+  }
+}
