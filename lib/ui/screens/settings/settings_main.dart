8989a3a59e5127b73057c7bdf680f030d52fdb4f
Hedon-dev <hedon-dev@hedon-haven.top>
2024-03-24 00:08:39 +0100

    Improve settings page + add about link

--- lib/ui/screens/settings/settings_main.dart
+++ lib/ui/screens/settings/settings_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hedon_viewer/ui/screens/about.dart';
 import 'package:hedon_viewer/ui/screens/settings/settings_appearance.dart';
 import 'package:hedon_viewer/ui/screens/settings/settings_homepage.dart';
 import 'package:hedon_viewer/ui/screens/settings/settings_plugins.dart';
@@ -15,47 +16,76 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
-        title: const Text("Settings"),
-      ),
-      body: SafeArea(
-          child: ListView(
-        children: <Widget>[
-          ListTile(
-            title: const Text("Plugins"),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => PluginsScreen()));
-            },
-          ),
-          ListTile(
-            title: const Text("Homepage"),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => HomepageScreen()));
-            },
-          ),
-          ListTile(
-            title: const Text("Appearance"),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const AppearanceScreen()));
-            },
-          ),
-          ListTile(
-            title: const Text("Video & Audio"),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const VideoAudioScreen()));
-            },
-          ),
-        ],
-      )),
-    );
+        appBar: AppBar(
+          iconTheme:
+              IconThemeData(color: Theme.of(context).colorScheme.primary),
+          title: Text("Settings",
+              style: Theme.of(context).textTheme.headlineLarge),
+        ),
+        body: SafeArea(
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: const Text("Plugins"),
+                    subtitle: const Text(
+                        "Enable/disable plugins, set plugin options"),
+                    leading: const Icon(Icons.extension),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const PluginsScreen()));
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("Homepage"),
+                    subtitle: const Text("Default provider settings"),
+                    leading: const Icon(Icons.home),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomepageScreen()));
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("Appearance"),
+                    subtitle: const Text("Default theme"),
+                    leading: const Icon(Icons.palette),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AppearanceScreen()));
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("Video & Audio"),
+                    subtitle: const Text(
+                        "Resolution, seek duration, player behavior"),
+                    leading: const Icon(Icons.headphones),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const VideoAudioScreen()));
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("About"),
+                    subtitle: const Text("About application"),
+                    leading: const Icon(Icons.info),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AboutScreen()));
+                    },
+                  )
+                ],
+              )),
+        ));
   }
 }
