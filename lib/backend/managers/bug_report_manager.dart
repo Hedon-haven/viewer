07610fec0adc5acb9c780ee4046fdace5b70a392
Hedon-dev <hedon-dev@hedon-haven.top>
2024-04-04 02:50:56 +0200

    Add debug screen

---
+++ lib/backend/managers/bug_report_manager.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class BugReportManager {
+  String? encodeQueryParameters(Map<String, String> params) {
+    return params.entries
+        .map((MapEntry<String, String> e) =>
+            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+        .join('&');
+  }
+
+  void submitReport(String submissionType, String issueType,
+      String generatedBody, String userInput) async {
+    switch (submissionType) {
+      case "Anonymous report":
+        // TODO: Set up anonymous reports
+        print("Not yet implemented");
+        break;
+      case "Private email report":
+        print("Opening email client");
+        await launchUrl(Uri(
+          scheme: 'mailto',
+          path: 'hedon-haven.7qw93@8shield.net',
+          query: encodeQueryParameters(<String, String>{
+            'subject': issueType,
+            'body': "$generatedBody\n\nAdditional information: \n$userInput"
+          }),
+        ));
+      case "Public github report":
+        // TODO: Set up different report templates on GitHub
+        // switch (issueType) {
+        //
+        // }
+        await Clipboard.setData(ClipboardData(
+            text: "$generatedBody\n\nAdditional information: \n$userInput"));
+        await launchUrl(
+            Uri.parse("https://github.com/hedon-haven/viewer/issues"));
+        break;
+    }
+  }
+}
