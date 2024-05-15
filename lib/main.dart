f7c63b396a16c91e2c9585a36ffa265826271533
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-05-15 06:06:54 +0200

    Add update mechanism

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hedon_viewer/backend/managers/database_manager.dart';
 import 'package:hedon_viewer/backend/managers/icon_manager.dart';
 import 'package:hedon_viewer/backend/managers/plugin_manager.dart';
 import 'package:hedon_viewer/backend/managers/shared_prefs_manager.dart';
+import 'package:hedon_viewer/backend/managers/update_manager.dart';
 import 'package:hedon_viewer/ui/screens/downloads.dart';
 import 'package:hedon_viewer/ui/screens/history.dart';
 import 'package:hedon_viewer/ui/screens/home.dart';
@@ -41,6 +42,12 @@ class ViewerAppState extends State<ViewerApp> {
   static final _defaultDarkColorScheme = ColorScheme.fromSwatch(
       primarySwatch: Colors.green, brightness: Brightness.dark);
 
+  bool updateAvailable = false;
+  bool isDownloadingUpdate = false;
+  String? updateLink;
+  double downloadProgress = 0.0;
+  UpdateManager updateManager = UpdateManager();
+
   int _selectedIndex = 0;
   static List<Widget> screenList = <Widget>[
     const HomeScreen(),
@@ -48,6 +55,29 @@ class ViewerAppState extends State<ViewerApp> {
     const DownloadsScreen(),
     const SettingsScreen(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    Future<String?> updateLinkFuture = updateManager.checkForUpdate();
+    updateLinkFuture.whenComplete(() async {
+      updateLink = await updateLinkFuture;
+      if (updateLink != null) {
+        setState(() {
+          updateAvailable = true;
+        });
+      }
+    });
+    updateManager.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    updateManager.removeListener(() {});
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,41 +91,80 @@ class ViewerAppState extends State<ViewerApp> {
           colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
         ),
         themeMode: SharedPrefsManager().getThemeMode(),
-        home: Scaffold(
-            bottomNavigationBar: NavigationBar(
-                destinations: <Widget>[
-                  NavigationDestination(
-                    icon: _selectedIndex == 0
-                        ? const Icon(Icons.home)
-                        : const Icon(Icons.home_outlined),
-                    label: 'Home',
-                  ),
-                  NavigationDestination(
-                    icon: _selectedIndex == 1
-                        ? const Icon(Icons.history)
-                        : const Icon(Icons.history_outlined),
-                    label: 'History',
-                  ),
-                  NavigationDestination(
-                    icon: _selectedIndex == 2
-                        ? const Icon(Icons.download)
-                        : const Icon(Icons.download_outlined),
-                    label: 'Downloads',
-                  ),
-                  NavigationDestination(
-                    icon: _selectedIndex == 3
-                        ? const Icon(Icons.settings)
-                        : const Icon(Icons.settings_outlined),
-                    label: 'Settings',
-                  ),
+        home: updateAvailable
+            ? AlertDialog(
+                title: const Text("Update available"),
+                content: SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    child: Column(children: [
+                  Text(!isDownloadingUpdate
+                      ? "Please install the update to continue"
+                      : "Downloading update..."),
+                  const SizedBox(height: 20),
+                  Expanded(child:
+                  isDownloadingUpdate
+                      ? LinearProgressIndicator(
+                          value: updateManager.downloadProgress)
+                      : const SizedBox())
+                ])),
+                actions: <Widget>[
+                  Center(
+                      child: !isDownloadingUpdate
+                          ? ElevatedButton(
+                              onPressed: () async {
+                                if (!isDownloadingUpdate) {
+                                  isDownloadingUpdate = true;
+                                  print("Startin download");
+                                  await updateManager
+                                      .downloadUpdate(updateLink!)
+                                      .whenComplete(() {
+                                    print("Ended download");
+                                    setState(() {
+                                      isDownloadingUpdate = false;
+                                    });
+                                  });
+                                }
+                              },
+                              child: const Text("Update and install"),
+                            )
+                          : const SizedBox()),
                 ],
-                selectedIndex: _selectedIndex,
-                onDestinationSelected: (index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                }),
-            body: screenList.elementAt(_selectedIndex)),
+              )
+            : Scaffold(
+                bottomNavigationBar: NavigationBar(
+                    destinations: <Widget>[
+                      NavigationDestination(
+                        icon: _selectedIndex == 0
+                            ? const Icon(Icons.home)
+                            : const Icon(Icons.home_outlined),
+                        label: 'Home',
+                      ),
+                      NavigationDestination(
+                        icon: _selectedIndex == 1
+                            ? const Icon(Icons.history)
+                            : const Icon(Icons.history_outlined),
+                        label: 'History',
+                      ),
+                      NavigationDestination(
+                        icon: _selectedIndex == 2
+                            ? const Icon(Icons.download)
+                            : const Icon(Icons.download_outlined),
+                        label: 'Downloads',
+                      ),
+                      NavigationDestination(
+                        icon: _selectedIndex == 3
+                            ? const Icon(Icons.settings)
+                            : const Icon(Icons.settings_outlined),
+                        label: 'Settings',
+                      ),
+                    ],
+                    selectedIndex: _selectedIndex,
+                    onDestinationSelected: (index) {
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                    }),
+                body: screenList.elementAt(_selectedIndex)),
       );
     });
   }
