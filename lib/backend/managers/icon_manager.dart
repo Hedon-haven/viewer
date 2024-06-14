1c883eb544746c079eba3a87a4e98e4b5c20b820
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-06-14 20:04:47 +0200

    Cleanup plugins and providers naming scheme
    Now plugins refers to the whole plugin, while provider is usually used in the context of HomepageProvider

--- lib/backend/managers/icon_manager.dart
+++ lib/backend/managers/icon_manager.dart
@@ -7,25 +7,26 @@ import 'package:path_provider/path_provider.dart';
 
 import 'plugin_manager.dart';
 
-/// Including provider images in the app itself might break copyright law -> download at first run and store locally
+/// Including plugin images in the app itself (or inside the third party plugin) might break copyright law
+/// -> download at first run and store locally
 // TODO: Every n-th run redownload the images?
 class IconManager {
-  void downloadProviderIcons() async {
-    print("Downloading provider icons");
+  void downloadPluginIcons() async {
+    print("Downloading plugin icons");
     Directory cacheDir = await getApplicationCacheDirectory();
-    for (PluginBase provider in PluginManager.allPlugins) {
-      Response response = await http.get(provider.pluginIconUri);
+    for (PluginBase plugin in PluginManager.allPlugins) {
+      Response response = await http.get(plugin.pluginIconUri);
       if (response.statusCode == 200) {
         print(
-            "Saving icon for ${provider.pluginName} to ${cacheDir.path}/${provider.pluginName}");
-        await File("${cacheDir.path}/${provider.pluginName}")
+            "Saving icon for ${plugin.pluginName} to ${cacheDir.path}/${plugin.pluginName}");
+        await File("${cacheDir.path}/${plugin.pluginName}")
             .writeAsBytes(response.bodyBytes);
       } else {
         print(
             "Error downloading icon: ${response.statusCode} - ${response.reasonPhrase}");
-        // use unknown provider instead of nothing
-        await File("assets/unknown-provider.png")
-            .copy("${cacheDir.path}/${provider.pluginName}");
+        // use unknown plugin instead of nothing
+        await File("assets/unknown-plugin.png")
+            .copy("${cacheDir.path}/${plugin.pluginName}");
       }
     }
   }
