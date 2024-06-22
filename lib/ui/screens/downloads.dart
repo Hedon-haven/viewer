1ccc5027c3ff231d90b84b949a7ca169a2edf580
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-06-22 16:11:16 +0200

    Remove unneeded code from downloads screen

--- lib/ui/screens/downloads.dart
+++ lib/ui/screens/downloads.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hedon_viewer/backend/managers/plugin_manager.dart';
 import 'package:hedon_viewer/backend/universal_formats.dart';
-import 'package:hedon_viewer/main.dart';
 
 class DownloadsScreen extends StatefulWidget {
   const DownloadsScreen({super.key});
@@ -14,22 +12,10 @@ class _DownloadsScreenState extends State<DownloadsScreen> {
   Future<List<UniversalSearchResult>> videoResults = Future.value([]);
 
   @override
-  void initState() {
-    super.initState();
-
-    if (sharedStorage.getBool("homepage_enabled")!) {
-      videoResults = PluginManager.getPluginByName(
-              sharedStorage.getStringList("homepage_providers")![0])!
-          .getHomePage(1);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
-        // TODO: add search to downloads
         actions: const [],
       ),
       body: const Text("Coming soon"),
