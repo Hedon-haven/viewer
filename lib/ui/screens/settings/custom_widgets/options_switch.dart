9c96f2a41db802daad4b675be1364f22053aafa5
Hedon-dev <hedon-dev@hedon-haven.top>
2024-09-13 23:26:20 +0200

    Add separate menu for provider settings of each plugin

--- lib/ui/screens/settings/custom_widgets/options_switch.dart
+++ lib/ui/screens/settings/custom_widgets/options_switch.dart
@@ -4,15 +4,14 @@ class OptionsSwitch extends StatefulWidget {
   final String title;
   final String subTitle;
   late bool switchState;
-  late bool showExtraHomeButton;
-  late bool homeButtonState;
+  late bool showSettingsButton;
   late bool reduceBorders;
 
   /// Make toggle visual only
   late bool nonInteractive;
   late Widget? leadingWidget;
   final void Function(bool) onToggled;
-  final void Function(bool) onToggledHomeButton;
+  final void Function() onPressedSettingsButton;
 
   OptionsSwitch(
       {super.key,
@@ -20,17 +19,15 @@ class OptionsSwitch extends StatefulWidget {
       required this.subTitle,
       required this.switchState,
       required this.onToggled,
-      bool? showExtraHomeButton,
-      bool? homeButtonState,
+      bool? showExtraSettingsButton,
       bool? reduceBorders,
       bool? nonInteractive,
       this.leadingWidget, // can be just null
-      void Function(bool)? onToggledHomeButton})
-      : showExtraHomeButton = showExtraHomeButton ?? false,
-        homeButtonState = homeButtonState ?? false,
+      void Function()? onPressedSettingsButton})
+      : showSettingsButton = showExtraSettingsButton ?? false,
         reduceBorders = reduceBorders ?? false,
         nonInteractive = nonInteractive ?? false,
-        onToggledHomeButton = onToggledHomeButton ?? ((_) {});
+        onPressedSettingsButton = onPressedSettingsButton ?? (() {});
 
   @override
   State<OptionsSwitch> createState() => _OptionsSwitchWidgetState();
@@ -52,31 +49,18 @@ class _OptionsSwitchWidgetState extends State<OptionsSwitch> {
             contentPadding: widget.reduceBorders ? EdgeInsets.zero : null,
           ),
         ),
-        widget.showExtraHomeButton
+        widget.showSettingsButton
             ? IconButton(
                 onPressed: () {
-                  // toggle homebutton state
-                  setState(() {
-                    widget.homeButtonState = !widget.homeButtonState;
-                  });
-
-                  widget.onToggledHomeButton(widget.homeButtonState);
+                  widget.onPressedSettingsButton();
                 },
-                icon: widget.homeButtonState
-                    ? const Icon(Icons.home)
-                    : const Icon(Icons.home_outlined))
+                icon: const Icon(Icons.settings))
             : const SizedBox(),
         Switch(
           value: widget.switchState,
           onChanged: (value) {
             widget.onToggled(value);
-
             if (!widget.nonInteractive) {
-              // toggle homepage accordingly
-              // this will not prevent the user from using just the homepage or just results
-              widget.onToggledHomeButton(value);
-              widget.homeButtonState = value;
-
               // The user provided function completes after the setState below
               // is called -> value is written to settings successfully,
               // but widget is not updated visually
