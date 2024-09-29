767f702e8ff7547e3e5e8ce4a3606793f10202dd
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-09-30 00:00:25 +0200

    Fix import path

--- lib/ui/screens/video_screen/video_screen.dart
+++ lib/ui/screens/video_screen/video_screen.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:auto_orientation/auto_orientation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hedon_viewer/ui/toast_notification.dart';
+import '/ui/toast_notification.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 import 'package:window_manager/window_manager.dart';
 
