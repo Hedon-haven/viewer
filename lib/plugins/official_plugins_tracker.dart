cb26d75280024796df166b8a5fcdc48cfefaf003
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-11-09 23:10:15 +0100

    Convert official_plugins_tracker.dart to imperative functions

--- lib/plugins/official_plugins_tracker.dart
+++ lib/plugins/official_plugins_tracker.dart
@@ -7,33 +7,30 @@ import 'official_plugins/tester.dart';
 import 'official_plugins/xhamster.dart';
 
 // While official plugins are also PluginInterface types, they in reality do not
-// communicate with any binaries, but are just directly implemented
-// Due to not being a third party plugin, they are not stored anywhere on the users device, but are compiled directly into the app
-class OfficialPluginsTracker {
-  /// Get an official Plugin (as Interface) by provider name
-  static PluginInterface? getPluginByName(String name) {
-    switch (name) {
-      case "Tester plugin":
-        if (!kDebugMode || !sharedStorage.getBool("enable_dev_options")!) {
-          logger.e("Tester plugin requested in non-debug mode");
-          throw Exception("Tester plugin requested in non-debug mode");
-        }
-        return TesterPlugin();
-      case "Pornhub.com":
-        return PornhubPlugin();
-      case "xHamster.com":
-        return XHamsterPlugin();
-      default:
-        break;
-    }
-    return null;
-  }
+// communicate with any binaries, but are compiled directly into the app
 
-  static List<PluginInterface> getAllPlugins() {
-    if (kDebugMode || sharedStorage.getBool("enable_dev_options")!) {
-      return [TesterPlugin(), PornhubPlugin(), XHamsterPlugin()];
-    } else {
-      return [PornhubPlugin(), XHamsterPlugin()];
-    }
+PluginInterface? getOfficialPluginByName(String name) {
+  switch (name) {
+    case "Tester plugin":
+      if (!kDebugMode || !sharedStorage.getBool("enable_dev_options")!) {
+        logger.e("Tester plugin requested in non-debug mode");
+        throw Exception("Tester plugin requested in non-debug mode");
+      }
+      return TesterPlugin();
+    case "Pornhub.com":
+      return PornhubPlugin();
+    case "xHamster.com":
+      return XHamsterPlugin();
+    default:
+      break;
+  }
+  return null;
+}
+
+List<PluginInterface> getAllOfficialPlugins() {
+  if (kDebugMode || sharedStorage.getBool("enable_dev_options")!) {
+    return [TesterPlugin(), PornhubPlugin(), XHamsterPlugin()];
+  } else {
+    return [PornhubPlugin(), XHamsterPlugin()];
   }
 }
