5e4b4b9b2c813b27d352bc9d3f61f5f6928f4163
Hedon-dev <hedon-dev@hedon-haven.top>
2024-11-19 18:14:55 +0100

    Create combined library screen + new subscriptions screen

---
+++ lib/ui/screens/library.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+import '/backend/managers/database_manager.dart';
+import '/backend/universal_formats.dart';
+import 'video_list.dart';
+
+class LibraryScreen extends StatefulWidget {
+  const LibraryScreen({super.key});
+
+  @override
+  State<LibraryScreen> createState() => _LibraryScreenState();
+}
+
+class _LibraryScreenState extends State<LibraryScreen> {
+  Future<List<UniversalSearchResult>> videoResults = Future.value([]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Padding(
+                padding: EdgeInsets.all(12),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: ListTile(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              tileColor:
+                              Theme.of(context).colorScheme.surfaceVariant,
+                              leading: Icon(Icons.history),
+                              trailing: Icon(Icons.arrow_forward),
+                              title: Text("History"),
+                              textColor: Theme.of(context)
+                                  .colorScheme
+                                  .onSurfaceVariant,
+                              titleTextStyle: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(fontWeight: FontWeight.bold),
+                              onTap: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                      const HistoryScreen())))),
+                      Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: ListTile(
+                            tileColor:
+                            Theme.of(context).colorScheme.surfaceVariant,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            leading: Icon(Icons.star),
+                            trailing: Icon(Icons.arrow_forward),
+                            title: Text("Favorites"),
+                            textColor:
+                            Theme.of(context).colorScheme.onSurfaceVariant,
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const FavoritesScreen())),
+                          )),
+                      Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: ListTile(
+                            tileColor:
+                            Theme.of(context).colorScheme.surfaceVariant,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            leading: Icon(Icons.download),
+                            trailing: Icon(Icons.arrow_forward),
+                            title: Text("Downloads"),
+                            textColor:
+                            Theme.of(context).colorScheme.onSurfaceVariant,
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                    const DownloadsScreen())),
+                          ))
+                    ]))));
+  }
+}
+
+class HistoryScreen extends StatefulWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
+        actions: const [],
+      ),
+      body: SafeArea(
+          child: VideoList(
+            videoResults: getWatchHistory(),
+            listType: "history",
+            loadingHandler: null,
+            searchRequest: null,
+          )),
+    );
+  }
+}
+
+class FavoritesScreen extends StatefulWidget {
+  const FavoritesScreen({super.key});
+
+  @override
+  State<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
+        actions: const [],
+      ),
+      body: SafeArea(
+          child: VideoList(
+            videoResults: getFavorites(),
+            listType: "favorites",
+            loadingHandler: null,
+            searchRequest: null,
+          )),
+    );
+  }
+}
+
+class DownloadsScreen extends StatefulWidget {
+  const DownloadsScreen({super.key});
+
+  @override
+  State<DownloadsScreen> createState() => _DownloadsScreenState();
+}
+
+class _DownloadsScreenState extends State<DownloadsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
+        actions: const [],
+      ),
+      body: SafeArea(child: Text("Downloads coming soon")),
+    );
+  }
+}
