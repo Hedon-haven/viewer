8ead01615ceee1631dfef94c562edfedcf80e046
Hedon-dev <hedon-dev@hedon-haven.top>
2024-12-03 13:19:16 +0100

    Add concealment screens

---
+++ lib/ui/screens/fake_apps/fake_reminders.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import '/main.dart';
+
+class FakeRemindersScreen extends StatefulWidget {
+  final Function parentStopConcealing;
+
+  const FakeRemindersScreen({super.key, required this.parentStopConcealing});
+
+  @override
+  State<FakeRemindersScreen> createState() => _FakeRemindersScreenState();
+}
+
+class _FakeRemindersScreenState extends State<FakeRemindersScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<String>?>(
+        future: sharedStorage.getStringList("fake_reminders_list"),
+        builder: (context, snapshot) {
+          // only build when data finished loading
+          if (snapshot.data == null) {
+            return const SizedBox();
+          }
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Reminders",
+                  style: Theme.of(context).textTheme.titleLarge),
+            ),
+            body: ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: ListTile(
+                    tileColor: Theme.of(context).colorScheme.surfaceVariant,
+                    textColor: Theme.of(context).colorScheme.onSurfaceVariant,
+                    title: Text(snapshot.data![index]),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () =>
+                          setState(() => snapshot.data!.removeAt(index)),
+                    ),
+                  )),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Add Reminder"),
+                      content: TextField(
+                        controller: _controller,
+                        decoration:
+                            const InputDecoration(hintText: "Enter reminder"),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context); // Close the dialog
+                          },
+                          child: const Text("Cancel"),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            if (_controller.text.trim().toLowerCase() == "stop concealing") {
+                              logger.i("Unconcealing app");
+                              widget.parentStopConcealing();
+                              Navigator.pop(context); // Close the dialog
+                              return;
+                            }
+                            if (_controller.text.isNotEmpty) {
+                              snapshot.data!.add(_controller.text);
+                              await sharedStorage.setStringList(
+                                  "fake_reminders_list", snapshot.data!);
+                              setState(() {});
+                              _controller.clear(); // Clear the text field
+                              Navigator.pop(context); // Close the dialog
+                            }
+                          },
+                          child: const Text("Add"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Icon(Icons.add),
+            ),
+          );
+        });
+  }
+}
---
+++ lib/ui/screens/fake_apps/fake_settings.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '/main.dart';
+import '/ui/screens/settings/custom_widgets/options_switch.dart';
+
+class FakeSettingsScreen extends StatefulWidget {
+  final Function parentStopConcealing;
+
+  const FakeSettingsScreen({super.key, required this.parentStopConcealing});
+
+  @override
+  State<FakeSettingsScreen> createState() => _FakeSettingsScreenState();
+}
+
+class _FakeSettingsScreenState extends State<FakeSettingsScreen> {
+  Timer? longPressTimer;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<String>?>(
+        future: sharedStorage.getStringList("fake_settings_list"),
+        builder: (context, snapshot) {
+          // only build when data finished loading
+          if (snapshot.data == null) {
+            return const SizedBox();
+          }
+          return Scaffold(
+              appBar: AppBar(
+                title: Text("Reminders",
+                    style: Theme.of(context).textTheme.titleLarge),
+              ),
+              body: ListView(children: [
+                OptionsSwitch(
+                    title: "Force preferred network type",
+                    switchState: snapshot.data![0] == "1",
+                    onToggled: (value) {
+                      snapshot.data![0] = value ? "1" : "0";
+                      sharedStorage.setStringList(
+                          "fake_settings_list", snapshot.data!);
+                      setState(() {});
+                    }),
+                OptionsSwitch(
+                    title: "Enable advanced mode",
+                    switchState: snapshot.data![1] == "1",
+                    onToggled: (value) {
+                      snapshot.data![1] = value ? "1" : "0";
+                      sharedStorage.setStringList(
+                          "fake_settings_list", snapshot.data!);
+                      setState(() {});
+                    }),
+                GestureDetector(
+                    onLongPressStart: (_) {
+                      logger.i("Long press started");
+                      longPressTimer = Timer(const Duration(seconds: 5), () {
+                        logger.i("Long press successful");
+                        logger.i("Unconcealing app");
+                        widget.parentStopConcealing();
+                      });
+                    },
+                    onLongPressEnd: (_) {
+                      logger.i("Long press ended prematurely. Not unconcealing.");
+                      longPressTimer?.cancel();
+                    },
+                    child: OptionsSwitch(
+                        title: "Show signal strength in advanced mode",
+                        switchState: snapshot.data![2] == "1",
+                        onToggled: (value) {
+                          snapshot.data![2] = value ? "1" : "0";
+                          sharedStorage.setStringList(
+                              "fake_settings_list", snapshot.data!);
+                          setState(() {});
+                        })),
+                OptionsSwitch(
+                    title: "Allow manual network selection in advanced mode",
+                    switchState: snapshot.data![3] == "1",
+                    onToggled: (value) {
+                      snapshot.data![3] = value ? "1" : "0";
+                      sharedStorage.setStringList(
+                          "fake_settings_list", snapshot.data!);
+                      setState(() {});
+                    }),
+                OptionsSwitch(
+                    title: "Enable always-on search",
+                    switchState: snapshot.data![4] == "1",
+                    onToggled: (value) {
+                      snapshot.data![4] = value ? "1" : "0";
+                      sharedStorage.setStringList(
+                          "fake_settings_list", snapshot.data!);
+                      setState(() {});
+                    }),
+              ]));
+        });
+  }
+}
