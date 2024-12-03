e4fa621f4de96a01be82d12387437a3ddd7c6554
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-12-03 14:45:23 +0100

    Only redownload icon cache every 5 runs

--- lib/backend/managers/icon_manager.dart
+++ lib/backend/managers/icon_manager.dart
@@ -9,33 +9,41 @@ import '/main.dart';
 import 'plugin_manager.dart';
 
 /// Including plugin images in the app itself (or inside the third party plugin) might break copyright law
-/// -> download at first run and store locally
-// TODO: Every n-th run re-download the images instead of downloading every time
-class IconManager {
-  void downloadPluginIcons() async {
-    logger.i("Downloading plugin icons");
-    Directory sysCache = await getApplicationCacheDirectory();
-    Directory cacheDir = Directory("${sysCache.path}/icons");
-    // Create icon cache dir if it doesn't exist
-    if (!cacheDir.existsSync()) {
-      cacheDir.create();
-    }
-    logger.d("Enabled plugins: ${PluginManager.enabledPlugins}");
-    for (PluginInterface plugin in PluginManager.enabledPlugins) {
-      try {
-        Response response = await http.get(plugin.iconUrl);
-        if (response.statusCode == 200) {
-          logger.d(
-              "Saving icon for ${plugin.codeName} to ${cacheDir.path}/${plugin.codeName}");
-          await File("${cacheDir.path}/${plugin.codeName}")
-              .writeAsBytes(response.bodyBytes);
-        } else {
-          logger.w(
-              "Error downloading icon: ${response.statusCode} - ${response.reasonPhrase}");
-        }
-      } catch (e) {
-        logger.w("Error downloading icon: $e");
+/// -> download at runtime and store in cache
+Future<void> downloadPluginIcons([bool force = false]) async {
+  int counter = (await sharedStorage.getInt("icon_cache_counter"))!;
+  if (counter != 5 && !force) {
+    logger.i("Icon cache counter is $counter (not 5). Skipping icon download.");
+    sharedStorage.setInt("icon_cache_counter", counter + 1);
+    return;
+  }
+  if (force) {
+    logger.i("Force downloading plugin icons");
+  }
+  logger.i("Icon cache counter is 5. Downloading plugin icons");
+  Directory sysCache = await getApplicationCacheDirectory();
+  Directory cacheDir = Directory("${sysCache.path}/icons");
+  // Create icon cache dir if it doesn't exist
+  if (!cacheDir.existsSync()) {
+    cacheDir.create();
+  }
+  logger.d("Enabled plugins: ${PluginManager.enabledPlugins}");
+  for (PluginInterface plugin in PluginManager.enabledPlugins) {
+    try {
+      Response response = await http.get(plugin.iconUrl);
+      if (response.statusCode == 200) {
+        logger.d(
+            "Saving icon for ${plugin.codeName} to ${cacheDir.path}/${plugin.codeName}");
+        await File("${cacheDir.path}/${plugin.codeName}")
+            .writeAsBytes(response.bodyBytes);
+      } else {
+        logger.w(
+            "Error downloading icon: ${response.statusCode} - ${response.reasonPhrase}");
       }
+    } catch (e) {
+      logger.w("Error downloading icon: $e");
     }
   }
+  logger.i("Resetting icon cache counter");
+  await sharedStorage.setInt("icon_cache_counter", 0);
 }
