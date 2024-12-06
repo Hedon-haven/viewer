ed203b62a39d0948441c267971a6363273aa472a
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2024-12-06 23:23:37 +0100

    Add launcher appearance to onboarding process

--- lib/ui/screens/settings/settings_launcher_appearance.dart
+++ lib/ui/screens/settings/settings_launcher_appearance.dart
@@ -1,12 +1,18 @@
 import 'package:dynamic_app_icon_flutter/dynamic_app_icon.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 import '/main.dart';
+import '/ui/screens/settings/settings_plugins.dart';
 import '/ui/toast_notification.dart';
 
 class LauncherAppearance extends StatefulWidget {
-  const LauncherAppearance({super.key});
+  final bool partOfOnboarding;
+  final void Function()? setStateMain;
+
+  const LauncherAppearance(
+      {super.key, this.partOfOnboarding = false, this.setStateMain});
 
   @override
   State<LauncherAppearance> createState() => _LauncherAppearanceScreenState();
@@ -31,17 +37,15 @@ class _LauncherAppearanceScreenState extends State<LauncherAppearance> {
             builder: (BuildContext context) {
               // running setupAppIcon will force the app to quit. Ask user to confirm first
               return AlertDialog(
-                  content: Text(
+                  backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+                  title: Text(
                       value == "Reminders"
                           ? "Create a new reminder called \"Stop concealing\" to exit reminders mode."
                           : "Long press on \"Show signal strength in advanced mode\" to exit GSM Settings mode",
-                      style: const TextStyle(fontWeight: FontWeight.bold)),
+                      style: Theme.of(context).textTheme.titleMedium),
                   actions: [
                     TextButton(
-                      onPressed: () {
-                        // close popup
-                        Navigator.pop(context);
-                      },
+                      onPressed: () => Navigator.pop(context),
                       child: const Text("Ok"),
                     )
                   ]);
@@ -53,9 +57,11 @@ class _LauncherAppearanceScreenState extends State<LauncherAppearance> {
           builder: (BuildContext context) {
             // running setupAppIcon will force the app to quit. Ask user to confirm first
             return AlertDialog(
+                backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
                 content: Text(
-                    "App will now close and can be found again as \"$value\" in the launcher.",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
+                    "App will now close and can be found again as \"$value\" in the launcher. "
+                    "${widget.partOfOnboarding ? "\n\nThis will also complete the onboarding process." : ""}",
+                    style: Theme.of(context).textTheme.titleMedium),
                 actions: [
                   TextButton(
                     onPressed: () {
@@ -100,64 +106,124 @@ class _LauncherAppearanceScreenState extends State<LauncherAppearance> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          // Hide back button in onboarding
+          automaticallyImplyLeading: !widget.partOfOnboarding,
           iconTheme:
               IconThemeData(color: Theme.of(context).colorScheme.primary),
-          title: const Text("App icon & name"),
+          title: widget.partOfOnboarding
+              ? Center(child: Text("Launcher appearance"))
+              : const Text("Launcher appearance"),
         ),
         body: SafeArea(
-            child: SingleChildScrollView(
-                child: FutureBuilder<String>(
-                    // Force non-null
-                    future: sharedStorage
-                        .getString("app_appearance")
-                        .then((value) => value!),
-                    builder: (context, snapshot) {
-                      // only build when data finished loading
-                      if (snapshot.data == null) {
-                        return const SizedBox();
-                      }
-                      return Column(
-                        children: [
-                          ListTile(
-                              title: const Text("Hedon haven"),
-                              leading: const CircleAvatar(
-                                foregroundImage: AssetImage(
-                                    "assets/launcher-icon/stock.png"),
-                                backgroundColor: Colors.white,
-                              ),
-                              trailing: Radio(
-                                value: "Hedon haven",
-                                groupValue: snapshot.data!,
-                                onChanged: handleOptionChange,
-                              )),
-                          const SizedBox(height: 10),
-                          ListTile(
-                              title: const Text("GSM Settings"),
-                              leading: const CircleAvatar(
-                                foregroundImage: AssetImage(
-                                    "assets/launcher-icon/fake_settings.png"),
-                                backgroundColor: Colors.white,
-                              ),
-                              trailing: Radio(
-                                value: "GSM Settings",
-                                groupValue: snapshot.data!,
-                                onChanged: handleOptionChange,
-                              )),
-                          const SizedBox(height: 10),
-                          ListTile(
-                              title: const Text("Reminders"),
-                              leading: const CircleAvatar(
-                                foregroundImage: AssetImage(
-                                    "assets/launcher-icon/reminders.png"),
-                                backgroundColor: Colors.white,
-                              ),
-                              trailing: Radio(
-                                value: "Reminders",
-                                groupValue: snapshot.data!,
-                                onChanged: handleOptionChange,
-                              ))
-                        ],
-                      );
-                    }))));
+            child: Column(children: [
+          SingleChildScrollView(
+              child: FutureBuilder<String>(
+                  // Force non-null
+                  future: sharedStorage
+                      .getString("launcher_appearance")
+                      .then((value) => value!),
+                  builder: (context, snapshot) {
+                    // only build when data finished loading
+                    if (snapshot.data == null) {
+                      return const SizedBox();
+                    }
+                    return Column(
+                      children: [
+                        ListTile(
+                            title: const Text("Hedon haven"),
+                            leading: const CircleAvatar(
+                              foregroundImage:
+                                  AssetImage("assets/launcher-icon/stock.png"),
+                              backgroundColor: Colors.white,
+                            ),
+                            trailing: Radio(
+                              value: "Hedon haven",
+                              groupValue: snapshot.data!,
+                              onChanged: handleOptionChange,
+                            )),
+                        const SizedBox(height: 10),
+                        ListTile(
+                            title: const Text("GSM Settings"),
+                            leading: const CircleAvatar(
+                              foregroundImage: AssetImage(
+                                  "assets/launcher-icon/fake_settings.png"),
+                              backgroundColor: Colors.white,
+                            ),
+                            trailing: Radio(
+                              value: "GSM Settings",
+                              groupValue: snapshot.data!,
+                              onChanged: handleOptionChange,
+                            )),
+                        const SizedBox(height: 10),
+                        ListTile(
+                            title: const Text("Reminders"),
+                            leading: const CircleAvatar(
+                              foregroundImage: AssetImage(
+                                  "assets/launcher-icon/reminders.png"),
+                              backgroundColor: Colors.white,
+                            ),
+                            trailing: Radio(
+                              value: "Reminders",
+                              groupValue: snapshot.data!,
+                              onChanged: handleOptionChange,
+                            ))
+                      ],
+                    );
+                  })),
+          if (widget.partOfOnboarding) ...[
+            Spacer(),
+            Padding(
+                padding: EdgeInsets.all(12),
+                child: Row(children: [
+                  Align(
+                      alignment: Alignment.bottomLeft,
+                      child: ElevatedButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.surfaceVariant),
+                          onPressed: () => Navigator.push(
+                              context,
+                              PageTransition(
+                                  type: PageTransitionType.leftToRightJoined,
+                                  childCurrent: widget,
+                                  child: PluginsScreen(
+                                      partOfOnboarding: true,
+                                      setStateMain: widget.setStateMain!))),
+                          child: Text("Back",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium
+                                  ?.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSurfaceVariant)))),
+                  Spacer(),
+                  Align(
+                      alignment: Alignment.bottomRight,
+                      child: ElevatedButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.primary),
+                          onPressed: () async {
+                            logger.i("Onboarding completed");
+                            await sharedStorage.setBool(
+                                "onboarding_completed", true);
+                            // Force redraw of main screen to exit onboarding
+                            widget.setStateMain!();
+                            // Go back to main screen
+                            await Navigator.pushNamedAndRemoveUntil(
+                                context, "/", (route) => false);
+                          },
+                          child: Text("Finish onboarding",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium
+                                  ?.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary))))
+                ]))
+          ]
+        ])));
   }
 }
