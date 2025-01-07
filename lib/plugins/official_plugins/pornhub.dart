d341bcb1a70e262aa8fd681b36d8ffca920bd5c3
Hedon-dev <hedon-dev@hedon-haven.top>
2025-01-07 15:45:07 +0100

    Log the uri with cookies in debug mode

--- lib/plugins/official_plugins/pornhub.dart
+++ lib/plugins/official_plugins/pornhub.dart
@@ -598,10 +598,11 @@ class PornhubPlugin extends PluginBase implements PluginInterface {
   @override
   Future<List<String>> getSearchSuggestions(String searchString) async {
     logger.d("Getting search suggestions for $searchString");
-    final response = await http.get(
-        Uri.parse(
-            "https://www.pornhub.com/video/search_autocomplete?&token=${_sessionCookies["token"]}&q=$searchString"),
-        headers: {"Cookie": "ss=${_sessionCookies["ss"]}"});
+    final Uri requestUri = Uri.parse(
+        "https://www.pornhub.com/video/search_autocomplete?&token=${_sessionCookies["token"]}&q=$searchString");
+    logger.d("Request URI: $requestUri with ss cookie: ${_sessionCookies["ss"]}");
+    final response = await http
+        .get(requestUri, headers: {"Cookie": "ss=${_sessionCookies["ss"]}"});
     Map<String, dynamic> data = jsonDecode(response.body);
     // The search results are just returned as key value pairs of numbers
     // e.g. {"0": "suggestion1", "1": "suggestion2", "2": "suggestion3"}
