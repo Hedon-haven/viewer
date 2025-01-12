603929fc31b91e8f815645b99b85c52e70a77ce1
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2025-01-12 19:07:34 +0100

    Allow requesting official plugin as OfficialPlugin (instead of the default PluginInterface)

--- lib/services/official_plugins_tracker.dart
+++ lib/services/official_plugins_tracker.dart
@@ -26,6 +26,25 @@ Future<PluginInterface?> getOfficialPluginByName(String codename) async {
   return null;
 }
 
+Future<OfficialPlugin?> getOfficialPluginByNameAsOfficialPlugin(
+    String codename) async {
+  switch (codename) {
+    case "tester-official":
+      if (!(await sharedStorage.getBool("enable_dev_options"))!) {
+        logger.e("Tester plugin requested in non-debug mode");
+        throw Exception("Tester plugin requested in non-debug mode");
+      }
+      return TesterPlugin();
+    case "pornhub-official":
+      return PornhubPlugin();
+    case "xhamster-official":
+      return XHamsterPlugin();
+    default:
+      break;
+  }
+  return null;
+}
+
 Future<List<PluginInterface>> getAllOfficialPlugins() async {
   if ((await sharedStorage.getBool("enable_dev_options"))!) {
     return [TesterPlugin(), PornhubPlugin(), XHamsterPlugin()];
