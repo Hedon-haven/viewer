7cf25d86d8cab4584ea3e50bb6223ec589a67858
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2025-02-03 22:13:58 +0100

    Do not allow dismissing update prompt

--- lib/ui/utils/update_dialog.dart
+++ lib/ui/utils/update_dialog.dart
@@ -15,96 +15,134 @@ void showUpdateDialog(UpdateManager updateManager, BuildContext parentContext) {
           // This is for showing the download progress
           updateManager.addListener(() => setState(() {}));
           return Scaffold(
-              body: AlertDialog(
-            backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
-            title: const Center(child: Text("Update available")),
-            content: Column(mainAxisSize: MainAxisSize.min, children: [
-              Padding(
-                  padding: !_updateFailed
-                      ? const EdgeInsets.only(bottom: 20)
-                      : EdgeInsets.zero,
-                  child: Text(
-                      _updateFailed
-                          ? "Update failed due to $_failReason\n\nPlease try again later."
-                          : _isDownloadingUpdate
-                              ? updateManager.downloadProgress == 0.0
-                                  ? "Fetching update metadata..."
-                                  : updateManager.downloadProgress == 1.0
-                                      ? "Installing update..."
-                                      : "Downloading update..."
-                              : "Please install the update to continue",
-                      style: Theme.of(context).textTheme.titleMedium)),
-              if (updateManager.latestChangeLog != null &&
-                  !_isDownloadingUpdate &&
-                  !_updateFailed) ...[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Latest changelog for ${updateManager.latestTag}: ",
-                        style: Theme.of(context).textTheme.titleMedium),
-                    const SizedBox(height: 5),
-                    Container(
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.surface,
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Text(updateManager.latestChangeLog!,
-                                style: Theme.of(context).textTheme.bodySmall)))
-                  ],
-                )
-              ],
-              if (_isDownloadingUpdate && !_updateFailed) ...[
-                LinearProgressIndicator(value: updateManager.downloadProgress)
-              ]
-            ]),
-            actions: <Widget>[
-              // This row is needed for the spacer to work
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                if (!_isDownloadingUpdate || _updateFailed) ...[
-                  ElevatedButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text(_updateFailed ? "Ok" : "Install later"),
-                  ),
-                  if (!_updateFailed) ...[
-                    Spacer(),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        // TODO: Fix background color of button
-                        backgroundColor: Theme.of(context).colorScheme.primary,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10),
-                      ),
-                      onPressed: () async {
-                        if (!_isDownloadingUpdate) {
-                          setState(() => _isDownloadingUpdate = true);
-                          logger.i("Starting update");
-                          try {
-                            await updateManager.downloadAndInstallUpdate(
-                                updateManager.latestTag!);
-                          } catch (e, stacktrace) {
-                            logger.e("Update failed with: $e\n$stacktrace");
-                            setState(() {
-                              _updateFailed = true;
-                              _failReason = e.toString();
-                            });
-                          }
-                        }
-                      },
-                      child: Text("Update and install",
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium!
-                              .copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.onPrimary)),
-                    )
-                  ]
-                ]
-              ])
-            ],
-          ));
+              body: PopScope(
+                  canPop: false,
+                  // Do not allow the user to close the dialog
+                  onPopInvoked: (_) {},
+                  child: AlertDialog(
+                    backgroundColor:
+                        Theme.of(context).colorScheme.surfaceVariant,
+                    title: Center(
+                        child: Text(_updateFailed
+                            ? "Update failed"
+                            : "Update available")),
+                    content: Column(mainAxisSize: MainAxisSize.min, children: [
+                      Padding(
+                          padding: !_updateFailed
+                              ? const EdgeInsets.only(bottom: 20)
+                              : EdgeInsets.zero,
+                          child: Text(
+                              _updateFailed
+                                  ? "Update failed due to $_failReason\n\nPlease try again later."
+                                  : _isDownloadingUpdate
+                                      ? updateManager.downloadProgress == 0.0
+                                          ? "Fetching update metadata..."
+                                          : updateManager.downloadProgress ==
+                                                  1.0
+                                              ? "Installing update..."
+                                              : "Downloading update..."
+                                      : "Please install the update to continue",
+                              style: Theme.of(context).textTheme.titleMedium)),
+                      if (updateManager.latestChangeLog != null &&
+                          !_isDownloadingUpdate &&
+                          !_updateFailed) ...[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                "Latest changelog for ${updateManager.latestTag}: ",
+                                style: Theme.of(context).textTheme.titleMedium),
+                            const SizedBox(height: 5),
+                            Container(
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).colorScheme.surface,
+                                  borderRadius: BorderRadius.circular(5.0),
+                                ),
+                                child: Padding(
+                                    padding: const EdgeInsets.all(5.0),
+                                    child: Text(updateManager.latestChangeLog!,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall)))
+                          ],
+                        )
+                      ],
+                      if (_isDownloadingUpdate && !_updateFailed) ...[
+                        LinearProgressIndicator(
+                            value: updateManager.downloadProgress)
+                      ]
+                    ]),
+                    actions: <Widget>[
+                      // This row is needed for the spacer to work
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            if (!_isDownloadingUpdate || _updateFailed) ...[
+                              ElevatedButton(
+                                style: TextButton.styleFrom(
+                                    backgroundColor: _updateFailed
+                                        ? Theme.of(context).colorScheme.primary
+                                        : Theme.of(context)
+                                            .colorScheme
+                                            .surface),
+                                onPressed: () => Navigator.pop(context),
+                                child: Text(
+                                    _updateFailed ? "Ok" : "Install later",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium!
+                                        .copyWith(
+                                            color: _updateFailed
+                                                ? Theme.of(context)
+                                                    .colorScheme
+                                                    .onPrimary
+                                                : Theme.of(context)
+                                                    .colorScheme
+                                                    .onSurface)),
+                              ),
+                              if (!_updateFailed) ...[
+                                Spacer(),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    // TODO: Fix background color of button
+                                    backgroundColor:
+                                        Theme.of(context).colorScheme.primary,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20, vertical: 10),
+                                  ),
+                                  onPressed: () async {
+                                    if (!_isDownloadingUpdate) {
+                                      setState(
+                                          () => _isDownloadingUpdate = true);
+                                      logger.i("Starting update");
+                                      try {
+                                        await updateManager
+                                            .downloadAndInstallUpdate(
+                                                updateManager.latestTag!);
+                                      } catch (e, stacktrace) {
+                                        logger.e(
+                                            "Update failed with: $e\n$stacktrace");
+                                        setState(() {
+                                          _updateFailed = true;
+                                          _failReason = "$e\n$stacktrace";
+                                        });
+                                      }
+                                    }
+                                  },
+                                  child: Text("Update and install",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium!
+                                          .copyWith(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .onPrimary)),
+                                )
+                              ]
+                            ]
+                          ])
+                    ],
+                  )));
         });
       });
 }
