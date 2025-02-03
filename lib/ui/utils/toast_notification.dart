f731346fce43e6313fd830ae91a0581d7e8c9958
Hedon-dev <hedon-dev@hedon-haven.top>
2025-02-03 21:15:39 +0100

    Add showToastViaOverlay

--- lib/ui/utils/toast_notification.dart
+++ lib/ui/utils/toast_notification.dart
@@ -24,4 +24,26 @@ class ToastMessageShower {
       foregroundColor: Theme.of(context).colorScheme.primary, // text
     );
   }
+
+  /// This does the same as [showToast] but uses [OverlayState] instead of [BuildContext]
+  static void showToastViaOverlay(String message, OverlayState overlay,
+      [int showDuration = 2]) {
+    logger.i("Showing overlay toast with message: $message");
+
+    // TODO: Get rid of white border color
+    toastification.show(
+      overlayState: overlay,
+      type: ToastificationType.info,
+      style: ToastificationStyle.simple,
+      title: Text(message, maxLines: 3),
+      alignment: Alignment.bottomCenter,
+      autoCloseDuration: Duration(seconds: showDuration),
+      animationDuration: const Duration(milliseconds: 500),
+      borderRadius: BorderRadius.circular(100.0),
+      closeButtonShowType: CloseButtonShowType.none,
+      backgroundColor: Theme.of(overlay.context).colorScheme.surface,
+      // background
+      foregroundColor: Theme.of(overlay.context).colorScheme.primary, // text
+    );
+  }
 }
