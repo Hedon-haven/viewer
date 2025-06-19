14fe32d69b95658970013b96015f6cf4b1d65f35
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2025-06-19 19:49:08 +0200

    Add support for single-message reports

--- lib/ui/screens/scraping_report.dart
+++ lib/ui/screens/scraping_report.dart
@@ -7,12 +7,14 @@ import '/utils/plugin_interface.dart';
 class ScrapingReportScreen extends StatefulWidget {
   final Map<PluginInterface, Map<String, List<dynamic>>> multiProviderMap;
   final Map<String, List<dynamic>> singleProviderMap;
+  final String? singleMessage;
   final Map<String, dynamic>? singleDebugObject;
 
   ScrapingReportScreen(
       {super.key,
       Map<PluginInterface, Map<String, List<dynamic>>>? multiProviderMap,
       Map<String, List<dynamic>>? singleProviderMap,
+      this.singleMessage,
       this.singleDebugObject})
       : multiProviderMap = multiProviderMap ?? {},
         singleProviderMap = singleProviderMap ?? {};
@@ -45,78 +47,106 @@ class _ScrapingReportScreenState extends State<ScrapingReportScreen> {
             title: Text("Scraping report",
                 style: Theme.of(context).textTheme.headlineLarge),
             actions: [
-              if (spm.isNotEmpty || mpm.isNotEmpty) ...[
+              if (spm.isNotEmpty ||
+                  mpm.isNotEmpty ||
+                  (widget.singleMessage != null &&
+                      widget.singleDebugObject != null)) ...[
                 Padding(
                     padding: const EdgeInsets.only(right: 16),
                     child: IconButton(
-                      icon: Stack(alignment: Alignment.bottomRight, children: [
-                        const Icon(Icons.send, size: 32),
-                        const Icon(Icons.done_all, size: 16)
-                      ]),
-                      onPressed: () {
-                        List<Map<String, dynamic>> combined = [];
-                        if (mpm.isNotEmpty) {
-                          for (int index = 0; index < mpm.length; index++) {
-                            List<Map<String, dynamic>> errors = [];
-                            for (var object
-                                in mpm[mpm.keys.elementAt(index)]!["Error"]!) {
-                              errors.add(object.convertToMap());
+                        icon:
+                            Stack(alignment: Alignment.bottomRight, children: [
+                          const Icon(Icons.send, size: 32),
+                          if (spm.isNotEmpty || mpm.isNotEmpty) ...[
+                            const Icon(Icons.done_all, size: 16)
+                          ]
+                        ]),
+                        onPressed: () {
+                          List<Map<String, dynamic>> combined = [];
+                          if (mpm.isNotEmpty) {
+                            for (int index = 0; index < mpm.length; index++) {
+                              List<Map<String, dynamic>> errors = [];
+                              for (var object in mpm[
+                                  mpm.keys.elementAt(index)]!["Error"]!) {
+                                errors.add(object.convertToMap());
+                              }
+                              List<Map<String, dynamic>> warnings = [];
+                              for (var object in mpm[
+                                  mpm.keys.elementAt(index)]!["Warning"]!) {
+                                errors.add(object.convertToMap());
+                              }
+                              // Only add plugins with errors
+                              if (!(mpm[mpm.keys.elementAt(index)]!["Critical"]!
+                                      .isEmpty &&
+                                  mpm[mpm.keys.elementAt(index)]!["Error"]!
+                                      .isEmpty &&
+                                  mpm[mpm.keys.elementAt(index)]!["Warning"]!
+                                      .isEmpty)) {
+                                combined.add({
+                                  "pluginCodeName":
+                                      mpm.keys.elementAt(index).codeName,
+                                  "criticalErrors": mpm[
+                                      mpm.keys.elementAt(index)]!["Critical"]!,
+                                  "errors": errors,
+                                  "warnings": warnings
+                                });
+                              }
                             }
-                            List<Map<String, dynamic>> warnings = [];
-                            for (var object in mpm[mpm.keys.elementAt(index)]![
-                                "Warning"]!) {
-                              errors.add(object.convertToMap());
-                            }
-                            // Only add plugins with errors
-                            if (!(mpm[mpm.keys.elementAt(index)]!["Critical"]!
-                                    .isEmpty &&
-                                mpm[mpm.keys.elementAt(index)]!["Error"]!
-                                    .isEmpty &&
-                                mpm[mpm.keys.elementAt(index)]!["Warning"]!
-                                    .isEmpty)) {
-                              combined.add({
-                                "pluginCodeName":
-                                    mpm.keys.elementAt(index).codeName,
-                                "criticalErrors": mpm[
-                                    mpm.keys.elementAt(index)]!["Critical"]!,
-                                "errors": errors,
-                                "warnings": warnings
-                              });
-                            }
+                          } else if (spm.isNotEmpty) {
+                            combined.add({
+                              "criticalErrors": spm["Critical"],
+                              "errors":
+                                  spm["Error"]?.map((e) => e.convertToMap()),
+                              "warnings":
+                                  spm["Warning"]?.map((e) => e.convertToMap()),
+                              "debugObject": widget.singleDebugObject ?? "null"
+                            });
                           }
-                        } else {
-                          combined.add({
-                            "criticalErrors": spm["Critical"],
-                            "errors":
-                                spm["Error"]?.map((e) => e.convertToMap()),
-                            "warnings":
-                                spm["Warning"]?.map((e) => e.convertToMap()),
-                            "debugObject": widget.singleDebugObject ?? "null"
-                          });
-                        }
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => BugReportScreen(
-                                    debugObject: combined,
-                                    issueType: "Plugin issue"))).then((value) {
-                          if (value) {
-                            // clear whole list
-                            setState(() => mpm.clear());
-                            setState(() => spm.clear());
-                            // Go back a screen
-                            Navigator.pop(context);
+
+                          if (spm.isNotEmpty || mpm.isNotEmpty) {
+                            Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => BugReportScreen(
+                                            debugObject: combined,
+                                            issueType: "Plugin issue")))
+                                .then((value) {
+                              if (value) {
+                                // clear whole list
+                                setState(() => mpm.clear());
+                                setState(() => spm.clear());
+                                // Go back a screen
+                                Navigator.pop(context);
+                              }
+                            });
+                          } else {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => BugReportScreen(
+                                            debugObject: [
+                                              widget.singleDebugObject!
+                                            ],
+                                            message: widget.singleMessage,
+                                            issueType: "Plugin issue")));
                           }
-                        });
-                      },
-                    ))
+                        }))
               ]
             ]),
         body: SafeArea(
             child: ListView(
                 padding: const EdgeInsets.all(8),
                 children: List.generate(mpm.isEmpty ? 1 : mpm.length, (index) {
-                  if (spm.isEmpty && mpm.isEmpty) {
+                  if (widget.singleMessage != null &&
+                      widget.singleDebugObject != null) {
+                    return Padding(
+                        padding: const EdgeInsets.only(top: 50),
+                        child: Center(
+                            child: Text(
+                                widget.singleMessage ?? "Unknown error message",
+                                style:
+                                    Theme.of(context).textTheme.titleLarge)));
+                  } else if (spm.isEmpty && mpm.isEmpty) {
                     return Padding(
                         padding: const EdgeInsets.only(top: 50),
                         child: Center(
