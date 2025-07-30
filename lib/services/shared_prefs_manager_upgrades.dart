ef279326ec8a7ebc0e11caf5c9b322bd017cae82
Hedon-dev <158850208+Hedon-dev@users.noreply.github.com>
2025-07-30 23:44:16 +0200

    Bump version

--- lib/services/shared_prefs_manager_upgrades.dart
+++ lib/services/shared_prefs_manager_upgrades.dart
@@ -41,6 +41,7 @@ Future<bool> startUpgrade(String currentVersion) async {
         await purgeDatabase();
         break;
       case "0.5.0":
+      case "0.5.1":
         break;
       default:
         logger.e("Unknown version: $currentVersion. Not changing anything");
